1c95b0ab9dea612f1b8473f352fa9a071fb6892d
João Ribeiro <joaosoft@gmail.com>
2018-04-12 23:07:18 +0100

    fix

--- service/global.go
+++ service/global.go
@@ -1,17 +1,8 @@
 package gomapper
 
 import (
-	"os"
-
 	"github.com/joaosoft/go-log/service"
-	"github.com/joaosoft/go-writer/service"
 )
 
 var global = make(map[string]interface{})
-var log = golog.NewLog(
-	golog.WithLevel(golog.InfoLevel),
-	golog.WithFormatHandler(gowriter.JsonFormatHandler),
-	golog.WithWriter(os.Stdout)).WithPrefixes(map[string]interface{}{
-	"level":   golog.LEVEL,
-	"time":    golog.TIME,
-	"service": "go-mapper"})
+var log = golog.NewLogDefault("go-mapper", golog.InfoLevel)
