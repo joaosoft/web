0f5100eec8a2790f3f02a4bbe30569867bf1aad0
João Ribeiro <joaosoft@gmail.com>
2018-08-27 23:44:49 +0100

    fix

--- main/main.go
+++ main/main.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-	"dependency"
+	"dependency/service"
 	"fmt"
 	"os"
 )
 
 func main() {
-	cmd := dependency.CmdDependencyGet
+	cmd := service.CmdDependencyGet
 
 	args := os.Args
 	if len(args) > 1 {
-		cmd = dependency.CmdDependency(args[1])
+		cmd = service.CmdDependency(args[1])
 	}
 
-	d, err := dependency.NewDependency()
+	d, err := service.NewDependency()
 	if err != nil {
 		panic(err)
 	}
 
 	switch cmd {
-	case dependency.CmdDependencyGet:
+	case service.CmdDependencyGet:
 		if err := d.Get(); err != nil {
 			panic(err)
 		}
-	case dependency.CmdDependencyUpdate:
+	case service.CmdDependencyUpdate:
 		if err := d.Update(); err != nil {
 			panic(err)
 		}
-	case dependency.CmdDependencyReset:
+	case service.CmdDependencyReset:
 		if err := d.Reset(); err != nil {
 			panic(err)
 		}
 	default:
-		fmt.Printf("invalid command! available commands are [%s, %s]", dependency.CmdDependencyGet, dependency.CmdDependencyReset)
+		fmt.Printf("invalid command! available commands are [%s, %s]", service.CmdDependencyGet, service.CmdDependencyReset)
 	}
 
 }
