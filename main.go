165896a11e2c5d48234fd3150fe2cd11aac37aef
João Ribeiro <joaosoft@gmail.com>
2018-08-31 00:09:38 +0100

    fix

--- main.go
+++ main.go
@@ -1,48 +1,47 @@
 package main
 
 import (
-	"dependency/service"
 	"fmt"
 	"os"
 )
 
 func main() {
-	cmd := service.CmdDependencyGet
-	protocol := service.ProtocolSSH
+	cmd := CmdDependencyGet
+	protocol := ProtocolSSH
 
 	args := os.Args
 	if len(args) > 1 {
-		cmd = service.CmdDependency(args[1])
+		cmd = CmdDependency(args[1])
 	}
 
 	if len(args) > 2 {
-		protocol = service.Protocol(args[2])
+		protocol = Protocol(args[2])
 	}
 
-	d, err := service.NewDependency(service.WithProtocol(protocol))
+	d, err := NewDependency(WithProtocol(protocol))
 	if err != nil {
 		panic(err)
 		os.Exit(1)
 	}
 
 	switch cmd {
-	case service.CmdDependencyGet:
+	case CmdDependencyGet:
 		if err := d.Get(); err != nil {
 			panic(err)
 			os.Exit(1)
 		}
-	case service.CmdDependencyUpdate:
+	case CmdDependencyUpdate:
 		if err := d.Update(); err != nil {
 			panic(err)
 			os.Exit(1)
 		}
-	case service.CmdDependencyReset:
+	case CmdDependencyReset:
 		if err := d.Reset(); err != nil {
 			panic(err)
 			os.Exit(1)
 		}
 	default:
-		fmt.Printf("invalid command! available commands are [%s, %s, %s]", service.CmdDependencyGet, service.CmdDependencyUpdate, service.CmdDependencyReset)
+		fmt.Printf("invalid command! available commands are [%s, %s, %s]", CmdDependencyGet, CmdDependencyUpdate, CmdDependencyReset)
 		os.Exit(1)
 	}
 
