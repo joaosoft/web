bf776410bb3d5311f4319729641f9f0f07bbcf06
João Ribeiro <joaosoft@gmail.com>
2018-10-05 01:08:13 +0100

    fix

---
+++ middleware_options.go
@@ -0,0 +1,21 @@
+package web
+
+import (
+	"strings"
+)
+
+func Options() MiddlewareFunc {
+	return func(next HandlerFunc) HandlerFunc {
+		return func(ctx *Context) error {
+
+			if ctx.Request.Method == MethodOptions {
+				if val, ok := ctx.Request.Headers[HeaderOrigin]; ok {
+					ctx.Response.Headers[HeaderAccessControlAllowOrigin] = val
+				}
+				ctx.Response.Headers[HeaderAccessControlAllowMethods] = []string{strings.Join(MethodsStr, " ")}
+			}
+
+			return next(ctx)
+		}
+	}
+}
