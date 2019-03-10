5f40ce1666ebcd4ccb554ce6a5f9279fa55f6afc
João Ribeiro <joao.ribeiro@foursource.pt>
2019-03-10 14:00:45 +0000

    fix

--- utils.go
+++ utils.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"mime"
+	"net"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -189,4 +190,18 @@ func NewSimpleConfig(file string, obj interface{}) error {
 		return err
 	}
 	return nil
+}
+
+func GetFreePort() (int, error) {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+	if err != nil {
+		return 0, err
+	}
+
+	l, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return 0, err
+	}
+	defer l.Close()
+	return l.Addr().(*net.TCPAddr).Port, nil
 }
