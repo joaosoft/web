4ce856c5aed038e0a55297b12613c7dcf350e11f
João Ribeiro <joao.ribeiro@foursource.pt>
2019-03-15 13:56:46 +0000

    fix

--- consts.go
+++ consts.go
@@ -5,5 +5,5 @@ const (
 	RFC850     = "Monday, 02-Jan-06 15:04:05 MST"
 	ANSIC      = "Mon Jan _2 15:04:05 2006"
 
-	RegexForURL  = "^((http|https)://)?(www)?[a-zA-Z0-9-._:/?&=,]+$"
+	RegexForURL = "^((http|https)://)?(www)?[a-zA-Z0-9-._:/?&=,%]+$"
 )
