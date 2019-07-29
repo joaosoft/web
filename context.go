fd41a3407ed28b6d9bf1554db3ad4998b3249d2f
João Ribeiro <joao.ribeiro@foursource.pt>
2019-07-29 09:24:40 +0100

    fix

---
+++ context.go
@@ -0,0 +1,23 @@
+package web
+
+import "fmt"
+
+func (ctx *Context) Redirect(host string) error {
+	if ctx.Request.Client == nil {
+		client, err := NewClient()
+		if err != nil {
+			return err
+		}
+
+		ctx.Request.Client = client
+	}
+
+	ctx.Request.Address = NewAddress(fmt.Sprintf("%s%s", host, ctx.Request.Address.Url))
+
+	response, err := ctx.Request.Send()
+	if err != nil {
+		return err
+	}
+
+	return ctx.Response.Bytes(response.Status, response.ContentType, response.Body)
+}
